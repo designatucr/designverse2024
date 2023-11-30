fe47e7fd9e524c5b624e1ef4347b8380ae18d4a4
Raidah Fairooz <rfair008@ucr.edu>
2023-11-29 17:09:38 -0800

    font work sans, logo pics

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,22 +2,24 @@
 "use client";
 import "./globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Poppins } from "next/font/google";
+// eslint-disable-next-line camelcase
+import { Work_Sans } from "next/font/google";
 import { SessionProvider } from "next-auth/react";
 import { Toaster } from "react-hot-toast";
 
-const poppins = Poppins({
+// eslint-disable-next-line camelcase
+const workSans = Work_Sans({
   subsets: ["latin"],
   display: "swap",
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  variable: "--font-poppins",
+  variable: "--font-work_sans",
 });
 
 export default function RootLayout({ children, session }) {
   return (
     <html lang="en" className="h-full">
       <body
-        className={`${poppins.variable} flex flex-col lg:flex-row bg-design-green-100 h-full w-full`}
+        className={`${workSans.variable} flex flex-col lg:flex-row bg-design-green-100 h-full w-full`}
       >
         <SessionProvider
           session={session}
