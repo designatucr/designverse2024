9dd7a82eaa5e223f6c9f8aeb83385b21c19bb29c
sharansingh1 <102877489+sharansingh1@users.noreply.github.com>
2023-12-04 15:15:49 -0800

    fixed bugs

--- src/data/dynamic/form/Volunteers.js
+++ src/data/dynamic/form/Volunteers.js
@@ -5,7 +5,7 @@ export const FIELDS = {
     input: "description",
     width: 12,
     texts: [
-      "Welcome to DesignVerse. Thank you for considering to become a volunteer, we appreciate your efforts to help support DesignVerse. On May 11, 2024 over the course of 24 hours, you’ll be able to challenge your problem solving skills, create innovative prototypes, and engage with a panel of industry experts!",
+      "Welcome to DesignVerse. Thank you for considering to become a volunteer, we appreciate your efforts to help support DesignVerse. DesignVerse is UCR’s first ever UI/UX Design-a-Thon! On May 11, 2024 over the course of 24 hours, you’ll be able to challenge your problem solving skills, create innovative prototypes, and engage with a panel of industry experts!",
       "Volunteers are not required to stay the full duration of the event, but are encouraged to checkout the various events, workshops, and opportunities that are available.",
       "Volunteer duties include but are not limited to setup, workshop organization, food handling, cleanup and more.",
       "Note: Volunteers are not permitted to become participants for the hackathon.",
