cf0a0ecf542fdb4ff20f92ad49e06b791d6ccafc
Raidah Fairooz <rfair008@ucr.edu>
2023-12-21 14:38:10 -0800

    large screen responsiveness, fix footer, faq border

--- src/components/static/upcoming/Upcoming.jsx
+++ src/components/static/upcoming/Upcoming.jsx
@@ -11,24 +11,27 @@ import LillypadwithLotus from "../../../../public/svgs/lotuswithpad.svg";
 import { IoPin } from "react-icons/io5";
 import { BsCalendar3 } from "react-icons/bs";
 import Logo from "../../../../public/svgs/BigLogo.svg";
-import Footer from "../Footer";
-import About from "../About";
-import FAQ from "../FAQ";
+// import Footer from "../Footer";
+// import About from "../About";
+// import FAQ from "../FAQ";
 
 const Upcoming = () => {
   return (
-    <div className="w-full h-screen overflow-x-hidden relative font-workSans">
+    <div className="w-full h-screen overflow-x-hidden overflow-y-hidden relative font-workSans">
       <div className="flex h-[68%] justify-between bg-gradient-to-b from-design-green-100 via-design-green-100 to-design-green-300">
         <div className="flex md:-mb-12 -mb-8 items-end md:items-end">
           <Image
-            className="flex absolute w-7/12 -ml-24 md:-ml-8 md:w-[28%] md:z-10"
+            className="flex absolute w-7/12 -ml-24 md:-ml-8 md:w-[28%] 2xl:w-1/4 md:z-10"
             src={Mushrooms2}
           />
         </div>
         <div className="absolute w-full flex flex-col justify-start items-center md:justify-start md:items-center mt-14">
-          <Image className="absolute flex w-4/6 md:w-3/12" src={Logo} />
-          <div className="flex w-5/6 md:w-3/5 rounded-3xl items-center md:items-center bg-design-green-400/70 mt-20 md:mt-24 flex-col">
-            <div className="flex text-white text-base md:text-2xl font-medium whitespace-normal mt-16">
+          <Image
+            className="absolute flex w-3/6 md:w-3/12 2xl:w-2/12"
+            src={Logo}
+          />
+          <div className="flex w-5/6 md:w-3/5 rounded-3xl items-center md:items-center bg-design-green-400/70 mt-14 md:mt-24 2xl:mt-32 flex-col">
+            <div className="flex text-white text-base md:text-2xl font-medium whitespace-normal mt-10 md:mt-16">
               <IoPin className="mr-0 md:mr-2 text-lg md:text-2xl" /> University
               of California Riverside
             </div>
@@ -48,47 +51,50 @@ const Upcoming = () => {
             <Apply text="Sponsor Us" link="/form/sponsor" />
           </div>
         </div>
-        <div className="w-4/12 flex flex-col h-full justify-end md:justify-right items-end md:items-right z-0 md:z-10 md:-mr-8">
-          <Image className="hidden absolute md:block" src={Animals} />
+        <div className="flex flex-col justify-end md:justify-right items-end md:items-right z-0 md:z-10 md:-mr-8">
+          <Image
+            className="hidden absolute md:block w-[27%] 2xl:w-3/12"
+            src={Animals}
+          />
         </div>
       </div>
       <Image className="bg-design-green-300 w-full border-0" src={Waterline} />
       <div className=" h-[32%] flex w-full bg-design-blue-200 relative">
-        <div className="w-4/12 md:w-1/6 justify-start absolute">
+        <div className="w-4/12 md:w-1/6 2xl:w-2/12 justify-start absolute">
           <Image
-            className="md:w-full -mt-2 md:-mt-20 md:ml-0 ml-16 animate-wiggle3"
+            className="md:w-full -mt-2 md:-mt-10 md:ml-0 ml-16 2xl:-mt-32 animate-wiggle3"
             src={OnlyFrog}
           />
         </div>
-        <div className="w-11/12 md:w-4/12 absolute bottom-2">
+        <div className="w-11/12 md:w-4/12 2xl:w-3/12 absolute bottom-2 2xl:bottom-20">
           <Image
-            className="w-full ml-40 md:ml-80 p-24 animate-wiggle2"
+            className="w-full ml-40 md:ml-80 2xl:ml-96 p-24 animate-wiggle2"
             src={Lillypad}
           />
         </div>
-        <div className="md:w-1/4 absolute -bottom-2">
+        <div className="md:w-1/4 2xl:w-2/12 absolute -bottom-2 2xl:bottom-4">
           <Image
-            className="hidden md:block w-full ml-60 p-24 animate-wiggle2"
+            className="hidden md:block w-full ml-60 2xl:ml-96 p-24 animate-wiggle2"
             src={Lotus}
           />
         </div>
 
-        <div className="md:w-3/12 absolute -bottom-5 -right-2">
+        {/* <div className="md:w-3/12 absolute -bottom-5 -right-2">
           <Image
             className="hidden md:block w-full p-24 animate-wiggle2"
             src={Lotus}
           />
-        </div>
-        <div className="md:w-3/12 absolute bottom-24 -right-2">
+        </div> */}
+        <div className="md:w-3/12 2xl:w-2/12 absolute bottom-24 -right-2">
           <Image
             className="hidden md:block w-full p-24 ml-32 animate-wiggle2"
             src={LillypadwithLotus}
           />
         </div>
       </div>
-      <About />
+      {/* <About />
       <FAQ />
-      <Footer />
+      <Footer /> */}
     </div>
   );
 };
