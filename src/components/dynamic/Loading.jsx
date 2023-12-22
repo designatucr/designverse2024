3036743e46557f327e015a8a1f379f8cbf35f558
sharansingh1 <102877489+sharansingh1@users.noreply.github.com>
2023-12-21 18:10:39 -0800

    loading

--- src/components/dynamic/Loading.jsx
+++ src/components/dynamic/Loading.jsx
@@ -2,9 +2,9 @@ import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 const Loading = () => {
   return (
-    <div className="w-full h-full flex flex-col items-center justify-center">
-      <p className="text-3xl font-bold text-hackathon-blue-100">Loading...</p>
-      <AiOutlineLoading3Quarters className="animate-spin text-hackathon-blue-100" />
+    <div className="w-full h-full flex flex-col items-center justify-center deisgn-white w-screen h-screen">
+      <p className="text-3xl font-bold text-black">Loading...</p>
+      <AiOutlineLoading3Quarters className="animate-spin text-black" />
     </div>
   );
 };
