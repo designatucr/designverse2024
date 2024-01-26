d103f750a76671b939df2a4c4c7959e6c30a783f
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-01-26 12:42:31 -0800

    updated tracks

--- src/components/static/Tracks.jsx
+++ src/components/static/Tracks.jsx
@@ -1,30 +1,27 @@
-import wave from "../../../public/svgs/wave.svg";
+import left from "../../../public/svgs/tracks/left.svg";
+import middle from "../../../public/svgs/tracks/middle.svg";
+import right from "../../../public/svgs/tracks/right.svg";
 import Image from "next/image";
 
 const Track = () => {
   return (
-    <div className="bg-design-blue-200 flex justify-between pb-28">
-      <div className="flex justify-center items-center flex-col w-full ">
-        <div className="text-5xl font-bold text-white">Tracks</div>
-
-        <div className="z-2 grid grid-cols-1 md:grid-cols-3 items-center justify-center mt-8 md:mt-24 gap-20">
-          <div className="w-40 md:w-60 h-40 md:h-60 rounded-full bg-design-green-light">
-            <p className="flex justify-center text-white text-md md:text-xl font-bold mt-44 md:mt-64">
-              Sustainability
-            </p>
+    <div className="bg-design-green-100 flex justify-between pb-28">
+      <div className="flex justify-center items-center flex-col w-full mt-8">
+        <div className="text-6xl font-bold text-white">Tracks</div>
+        <div className="grid grid-cols-1 md:grid-cols-3 items-center justify-center text-white text-3xl font-bold mt-5 gap-4">
+          <div className="flex flex-col">
+            <Image src={left} alt="left" />
+            <p className="flex justify-center md:ml-10">Sustainability</p>
           </div>
-          <div className="w-40 md:w-60 h-40 md:h-60 rounded-full bg-design-green-light">
-            <p className="flex justify-center text-white text-md md:text-xl font-bold mt-44 md:mt-64">
-              Sustainability
-            </p>
+          <div className="flex flex-col mt-2">
+            <Image src={middle} alt="middle" className="mt-4" />
+            <p className="flex justify-center md:mr-8 mt-0 ">Sustainability</p>
           </div>
-          <div className="w-40 md:w-60 h-40 md:h-60 rounded-full bg-design-green-light">
-            <p className="flex justify-center text-white text-md md:text-xl font-bold mt-44 md:mt-64">
-              Sustainability
-            </p>
+          <div className="flex flex-col">
+            <Image src={right} alt="right" />
+            <p className="flex justify-center md:mr-12">Sustainability</p>
           </div>
         </div>
-        <Image src={wave} alt="wave" className="z-1 absolute hidden md:block" />
       </div>
     </div>
   );
