aedf52f7eacbcd45173d3026551b6bd09d72af39
Raidah Fairooz <rfair008@ucr.edu>
2024-02-06 15:04:59 -0800

    river fixed

--- src/components/static/Tracks.jsx
+++ src/components/static/Tracks.jsx
@@ -1,11 +1,12 @@
-import left from "../../../public//svgs/tracks/left.svg";
+import left from "../../../public/svgs/tracks/left.svg";
 import middle from "../../../public/svgs/tracks/middle.svg";
 import right from "../../../public/svgs/tracks/right.svg";
 import Image from "next/image";
-
+import top from "../../../public/svgs/tracks/track.svg";
 const Track = () => {
   return (
-    <div className="bg-design-green-100 flex justify-between">
+    <div className="bg-design-green-100 flex flex-col justify-between">
+      <Image src={top} className="bg-design-blue-200" />
       <div className="flex justify-center items-center flex-col w-full my-8">
         <div className="text-5xl font-bold text-white">TRACKS</div>
         <div className="grid grid-cols-1 md:grid-cols-3 w-11/12  text-white text-2xl font-bold mt-5 ">
