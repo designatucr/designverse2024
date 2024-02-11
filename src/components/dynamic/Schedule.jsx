23c022be03479f84781805753aeccb273d37106b
fardinzam <fardinzaman51@gmail.com>
2024-02-10 21:36:43 -0800

    updated schedule

--- src/components/dynamic/Schedule.jsx
+++ src/components/dynamic/Schedule.jsx
@@ -6,10 +6,15 @@ import LOGEND from "@/public/svgs/schedule/logEnd.svg";
 import LOGSTART from "@/public/svgs/schedule/logStart.svg";
 import WAVES from "@/public/svgs/wave.svg";
 import LILYPAD from "@/public/svgs/landing/lillypad.svg";
-import LOTUSLILY from "@/public/svgs/landing/lotuswithpad.svg";
 import FROG from "@/public/svgs/landing/onlyFrog.svg";
+import twiggy1 from "@/public/svgs/schedule/twiggy1.svg";
+import twiggy2 from "@/public/svgs/schedule/twiggy2.svg";
 
 const days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+function getRandomFromArray(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
 const Schedule = () => {
   const [events, setEvents] = useState([]);
   const [selectedDay, setSelectedDay] = useState("SUN");
@@ -22,11 +27,13 @@ const Schedule = () => {
       setEvents([]);
     }
   }, [selectedDay]);
+  const randomXPositions = [10, 16, 20, 28, 36, 44, 52, 60, 72, 90];
+  const randomXPositions2 = [10, 14, 24, 32, 40, 48, 56, 64, 80];
 
   console.log("EVENTS:", events);
   return (
     <div className="bg-design-blue-200 pt-10 relative">
-      <div className="w-9/12 mx-auto text-white font-workSans flex items-center">
+      <div className="w-9/12 flex mx-auto text-white font-workSans items-center">
         <div className="w-full text-center z-1">
           <p className="text-3xl lg:text-5xl font-bold inline-block">
             SCHEDULE
@@ -35,11 +42,11 @@ const Schedule = () => {
             Pacific Standard Time (PST)
           </p>
           <div className="mt-4">
-            <div className="w-fit mx-auto flex justify-center bg-design-blue-100 rounded">
+            <div className="text-sm md:text-base w-fit mx-auto flex bg-design-blue-100 rounded">
               {days.map((day) => (
                 <button
                   key={day}
-                  className={`flex py-2 px-3 md:px-4 justify-between rounded focus:outline-none text-white ${
+                  className={`flex py-2 px-3 justify-between rounded focus:outline-none text-white ${
                     selectedDay === day
                       ? "bg-design-blue-400"
                       : "bg-transparent"
@@ -52,41 +59,58 @@ const Schedule = () => {
             </div>
             <div className="mt-6 h-[1200px] relative">
               {events &&
-                events.map((event, index) => (
-                  <div key={index} className="flex">
-                    <Image
-                      src={LOGSTART}
-                      className="w-[16.6px] flex relative"
-                      alt="Log"
-                    />
-                    <div
-                      key={event.name}
-                      className="my-4 w-full flex justify-between bg-[#695546]"
-                    >
-                      <p className="text-lg m-0 py-3 pl-6 lg:pl-16">{`${event.TIME}`}</p>
-                      <p className="text-lg m-0 py-3">{`${event.EVENT}`}</p>
-                      <p className="text-lg m-0 py-3">{`${event.TYPE}`}</p>
-                      <p className="text-lg m-0 py-3 pr-6 lg:pr-16">{`${event.LOCATION}`}</p>
+                events.map((event, index) => {
+                  const randomX1 = getRandomFromArray(randomXPositions);
+                  const randomX2 = getRandomFromArray(randomXPositions2);
+
+                  return (
+                    <div key={index}>
+                      <Image
+                        src={twiggy2}
+                        className="w-6 h-6 relative top-2"
+                        style={{ left: `${randomX1}%` }}
+                      />
+                      <div className="flex w-full">
+                        <Image
+                          src={LOGSTART}
+                          className="w-[13.3px] md:w-[16.6px] flex relative"
+                          alt="Log"
+                        />
+                        <div
+                          key={event.name}
+                          className="w-full flex justify-between bg-[#695546]"
+                        >
+                          <p className="text-xs md:text-lg m-0 py-3 pl-6 lg:pl-16">{`${event.TIME}`}</p>
+                          <p className="text-xs md:text-lg m-0 py-3">{`${event.EVENT}`}</p>
+                          <p className="text-xs md:text-lg m-0 py-3">{`${event.TYPE}`}</p>
+                          <p className="text-xs md:text-lg m-0 py-3 pr-6 lg:pr-16">{`${event.LOCATION}`}</p>
+                        </div>
+                        <Image
+                          src={LOGEND}
+                          className="w-[6.53px] md:w-[8.15px] flex relative"
+                          alt="Log"
+                        />
+                      </div>
+                      <Image
+                        src={twiggy1}
+                        className="w-6 h-6 relative bottom-2 rotate-180"
+                        style={{ left: `${randomX2}%` }}
+                      />
                     </div>
-                    <Image
-                      src={LOGEND}
-                      className="w-[8.15px] flex relative"
-                      alt="Log"
-                    />
-                  </div>
-                ))}
+                  );
+                })}
             </div>
           </div>
         </div>
       </div>
       <Image
         src={WAVES}
-        className="absolute w-screen flex z-0 top-16"
+        className="absolute w-90 margin-auto z-0 top-16 flex-none"
         alt="Waves"
       />
       <Image
         src={WAVES}
-        className="absolute w-screen flex z-0 top-[60%] transform -scale-x-100 -scale-y-100"
+        className="absolute min-w-96 z-0 bottom-32 md:bottom-16 rotate-180"
         alt="Waves"
       />
       <Image
@@ -94,11 +118,11 @@ const Schedule = () => {
         className="absolute w-screen flex z-0 h-16 top-[30%] left-[47%] transform -scale-x-100"
         alt=""
       />
-      <Image
+      {/* <Image
         src={LOTUSLILY}
-        className="absolute w-screen flex z-0 w-24 bottom-96  left-[-2%]"
+        className="absolute w-screen flex z-0 w-32 bottom-96  left-[-2%]"
         alt="Lotus on lilypad"
-      />
+      /> */}
       <Image
         src={FROG}
         className="absolute w-screen flex z-0 h-24 bottom-4 left-[42%]"
