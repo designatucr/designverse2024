76057226f40668dbcff65a69c21367f39d20d554
menthy-wu <wumengxuan0728@gmail.com>
2024-02-20 10:26:09 -0800

    add alt

--- src/components/static/Footer.jsx
+++ src/components/static/Footer.jsx
@@ -10,17 +10,19 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <div className="flex justify-between bg-design-blue-300 text-white relative">
-      <Image src={mushroomsLeft} className="w-4/12 md:w-3/12 z-10" />
+      <Image
+        src={mushroomsLeft}
+        className="w-4/12 md:w-3/12 z-10"
+        alt="mushroomsLeft"
+      />
       <div className="flex flex-col items-center justify-end w-3/12 md:w-full z-10">
-        <Image src={logo} width={100} />
+        <Image src={logo} width={100} alt="logo" />
 
         <div className="flex space-x-1 md:gap-5 w-full justify-center items-center mt-4">
           <Link href="mailto:designverseucr@gmail.com ">
-            {" "}
             <IoIosMail className="text-4xl hover:cursor-pointer text-white" />{" "}
           </Link>
           <Link href="https://www.instagram.com/designverseucr/">
-            {" "}
             <RiInstagramFill className="text-3xl hover:cursor-pointer text-white" />
           </Link>
           <Link href="https://www.linkedin.com/company/designverseucr">
@@ -35,9 +37,17 @@ const Footer = () => {
           &nbsp;by the DesignVerse Team 2024
         </div>
       </div>
-      <Image src={mushroomsRight} className="w-4/12 md:w-3/12 z-10" />
+      <Image
+        src={mushroomsRight}
+        className="w-4/12 md:w-3/12 z-10"
+        alt="mushroomsRight"
+      />
       <div className=" flex w-full absolute mt-14 z-0 bottom-0">
-        <Image src={FooterWithoutEmoji} className="w-full" />
+        <Image
+          src={FooterWithoutEmoji}
+          className="w-full"
+          alt="FooterWithoutEmoji"
+        />
       </div>
     </div>
   );
