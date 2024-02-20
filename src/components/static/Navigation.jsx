568acf0584f7925654ed93b3baa0e7fc7bd218ea
Raidah Fairooz <rfair008@ucr.edu>
2024-02-20 14:29:28 -0800

    small bugs, sponsor still broken

--- src/components/static/Navigation.jsx
+++ src/components/static/Navigation.jsx
@@ -25,8 +25,8 @@ const Navigation = () => {
           className={`${
             isDropdownOpen
               ? "opacity-0 duration-300"
-              : "opacity-100 bg-design-green-300 duration-300"
-          }bg-design-green-300 rounded-xl w-1/4 pl-4 py-2.5 drop-shadow-xl`}
+              : "opacity-100 bg-design-green-200 duration-300"
+          }bg-design-green-200 rounded-xl w-1/4 pl-4 py-2.5 drop-shadow-xl`}
         >
           <FaBars className="text-white text-lg drop-shadow-md" />
         </button>
@@ -35,7 +35,7 @@ const Navigation = () => {
             isDropdownOpen
               ? "transition-all duration-300 ease-out transform translate-y-0 opacity-100"
               : "transition-all duration-300 ease-in  transform -translate-x-96 opacity-0"
-          } bg-design-green-300 p-2 ml-0 -mt-10 grid grid-col rounded-xl drop-shadow-lg `}
+          } bg-design-green-200 p-2 ml-0 -mt-10 grid grid-col rounded-xl drop-shadow-lg `}
         >
           <button onClick={closeNavbar}>
             <FaChevronLeft className="flex text-white text-xl items-center justify-start my-1 ml-1" />
@@ -52,7 +52,7 @@ const Navigation = () => {
               // smooth={true}
               // offset={0}
               // duration={250}
-              className="flex px-2 py-1.5 text-white text-lg flex-cols gap-3 justify-start items-center no-underline hover:cursor-pointer duration-500"
+              className="flex px-2 py-1.5 text-white text-lg flex-cols gap-3 justify-start items-center no-underline hover:cursor-pointer hover:bg-design-green-300 duration-500"
             >
               {item.icon}
               {item.name}
