29ee0213cb243ecb49cc9962bec3fa0cb173b5b6
shahdivyank <divyank.shah.2016@gmail.com>
2024-04-01 17:25:42 -0700

    add debugging

--- src/components/static/schedule/Schedule.jsx
+++ src/components/static/schedule/Schedule.jsx
@@ -1,20 +1,37 @@
+"use client";
 import { api } from "@/utils/api";
 import Events from "./Events";
+import { useEffect, useState } from "react";
 
-const Schedule = async () => {
-  const { items } = await api({
-    url: `https://www.googleapis.com/calendar/v3/calendars/${process.env.NEXT_PUBLIC_GOOGLE_CALENDAR}/events?key=${process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_API_KEY}&singleEvents=true&orderBy=startTime`,
-    method: "GET",
-  });
+const Schedule = () => {
+  const [events, setEvents] = useState([
+    {
+      start: new Date(),
+      description: "",
+    },
+  ]);
 
-  items.forEach((event) => {
-    event.start = new Date(event.start.dateTime);
-    event.end = new Date(event.end.dateTime);
-  });
+  useEffect(() => {
+    const load = async () => {
+      const { items } = await api({
+        url: `https://www.googleapis.com/calendar/v3/calendars/${process.env.NEXT_PUBLIC_GOOGLE_CALENDAR}/events?key=${process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_API_KEY}&singleEvents=true&orderBy=startTime`,
+        method: "GET",
+      });
 
-  const totalDays = [...new Set(items.map(({ start }) => start.getDay()))];
+      items.forEach((event) => {
+        event.start = new Date(event.start.dateTime);
+        event.end = new Date(event.end.dateTime);
+      });
 
-  return <Events events={items} totalDays={totalDays} />;
+      setEvents(items);
+    };
+
+    load();
+  }, []);
+
+  const totalDays = [...new Set(events.map(({ start }) => start.getDay()))];
+
+  return <Events events={events} totalDays={totalDays} />;
 };
 
 export default Schedule;
