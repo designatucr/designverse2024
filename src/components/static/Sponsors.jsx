327f636ba6567de785ad45ca60207412e7442920
shahdivyank <divyank.shah.2016@gmail.com>
2024-04-29 18:00:51 -0700

    add sponsor

--- src/components/static/Sponsors.jsx
+++ src/components/static/Sponsors.jsx
@@ -5,6 +5,7 @@ import bear from "@/public/svgs/sponsors/bear.svg";
 import chicken from "@/public/svgs/sponsors/chicken.svg";
 import cloud from "@/public/svgs/sponsors/cloud.svg";
 import Link from "next/link";
+
 const Sponsors = () => {
   return (
     <div
@@ -34,32 +35,25 @@ const Sponsors = () => {
       <div className="text-3xl lg:text-5xl font-bold text-newdesign-cyan-200 mb-10">
         SPONSORS
       </div>
-      {Array(3)
-        .fill()
-        .map((_, index) => (
-          <div
-            key={index}
-            className="flex items-center justify-center space-x-4 lg:space-x-24 w-full mb-12"
-          >
-            {Array(index === 2 ? 4 : index + 1)
-              .fill()
-              .map((_, idx) => (
-                <Link
-                  key={idx}
-                  href={SPONSORS[index].link}
-                  className="bg-newdesign-cyan-200 w-[25%] lg:w-[10%] aspect-square rounded-full flex items-center justify-center p-3 hover:cursor-pointer hover:scale-105"
-                >
-                  <Image
-                    src={
-                      SPONSORS[Math.floor(((index + 1) / 2) * index) + idx].pic
-                    }
-                    alt="carousel"
-                    className="w-full"
-                  />
-                </Link>
-              ))}
-          </div>
-        ))}
+
+      {SPONSORS.map((tiers, index) => (
+        <div
+          key={index}
+          className="flex items-center justify-center space-x-4 lg:space-x-24 w-full mb-12"
+        >
+          {tiers.map(({ link, pic }, i) => (
+            <Link
+              key={i}
+              href={link}
+              className={`bg-newdesign-cyan-200 w-[25%] ${
+                index === 0 || index === 1 ? "lg:w-[12%]" : "lg:w-[9%]"
+              }  aspect-square rounded-full flex items-center justify-center p-3 hover:cursor-pointer hover:scale-105`}
+            >
+              <Image src={pic} alt="carousel" className="w-full" />
+            </Link>
+          ))}
+        </div>
+      ))}
     </div>
   );
 };
