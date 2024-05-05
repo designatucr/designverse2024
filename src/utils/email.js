67a5e28127550580d0be1db39eed24e2ebce8e2b
shahdivyank <divyank.shah.2016@gmail.com>
2024-05-05 15:51:09 -0700

    revert

--- src/utils/email.js
+++ src/utils/email.js
@@ -6,7 +6,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const send = async ({ email, id, name, position, subject, preview }) => {
   const { data, error } = await resend.emails.send({
     from: "DesignVerse <info@designverseucr.org>",
-    to: ["designverseucr@gmail.com"],
+    to: [email],
     subject: subject,
     // eslint-disable-next-line new-cap
     react: Email({ id, name, position, preview }),
